87b5c285a512b39bf1dac9ca49d1bd9aee3eee68
masktheking <100652721+masktheking@users.noreply.github.com>
2022-03-18 00:30:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,14 +32,14 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 🧑🏻‍💻 **Developer:** @MaskTHeKing
 
-👥 **:** 
+ **:** 
 
-📢 **:** 
+ **:** 
 """
 	ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:** @MaskTheKing
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
+Developer is Super Noob. Just Learning from Official Docs.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
